09bfd778ce9a90a15dcdc13ec79088b6b2899f1b
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2019-11-11 09:45:48 +0800

    Sort Contacts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from 'react';
+import { orderBy } from 'lodash';
 import './primitive.css';
 import ContactList from './contacts/list';
 import AddContact from './contacts/add';
 import EditContact from './contacts/edit';
 import { Contact, initialState } from './contacts/model';
 
+interface Sort {
+  type: "asc" | "desc";
+  field: string;
+}
+
 const App: React.FC = () => {
   const data: Contact[] = [
     { id: 1, name: 'Tania', email: 'tania@mail.com', contactNumber: '123123123' },
-    { id: 2, name: 'Craig', email: 'craig@email.com', contactNumber: '123123123' },
-    { id: 3, name: 'Ben', email: 'ben@email.com', contactNumber: '123123123' },
+    { id: 2, name: 'Craig', email: 'craig@email.com', contactNumber: '6546456' },
+    { id: 3, name: 'Ben', email: 'ben@email.com', contactNumber: '234234' },
   ]
 
   const [contacts, setContacts] = useState(data);
+  const [sort, setSort] = useState<Sort>({ type: "asc", field: "name"});
 
   const addContact = (contact: Contact) => {
     contact.id = contacts.length + 1;
-    setContacts([...contacts, contact]);
+    setContacts(orderBy([...contacts, contact], [sort.field],[sort.type]));
   }
 
   const deleteContact = (id?:number) => {
     setContacts(contacts.filter(contact => contact.id !== id));
+  }
+
+  const sortContacts = (sort: "desc" | "asc", field: string) => {
+    const sortedContacts = orderBy(contacts, [field],[sort]); // Use Lodash to sort array by 'name'
+    setContacts(sortedContacts)
   }
 
   const [editing, setEditing] = useState(false);
@@ -45,7 +57,7 @@ const App: React.FC = () => {
       <div className="flex-row">
         {editing ? (
           <div className="flex-large">
-            <h2>Edit user</h2>
+            <h2>Edit Contact</h2>
             <EditContact
               editing={editing}
               setEditing={setEditing}
@@ -55,16 +67,19 @@ const App: React.FC = () => {
           </div>
         ) : (
           <div className="flex-large">
-            <h2>Add user</h2>
+            <h2>Add Contact</h2>
             <AddContact addContact={addContact} />
           </div>
         )}
         <div className="flex-large">
           <h2>View Contacts</h2>
           <ContactList 
-            contacts={contacts} 
+            contacts={contacts}
+            sortContacts={sortContacts}
             editContact={editContact}
             deleteContact={deleteContact}
+            sort={sort}
+            setSort={setSort}
           />
         </div>
       </div>
--- src/contacts/list.tsx
+++ src/contacts/list.tsx
@@ -1,43 +1,79 @@
 import React from 'react';
+import { FaSortUp, FaSortDown } from 'react-icons/fa';
 import { Contact } from './model';
+
+interface Sort {
+  type: "asc" | "desc";
+  field: string;
+}
 
 export interface Props {
     contacts: Contact[];
     deleteContact: (id?:number) => void;
     editContact: (contact:Contact) => void;
+    sortContacts: (sort: "asc" | "desc", field: string) => void;
+    sort: Sort;
+    setSort: (sort: Sort) => void;
 }
 
-const ContactList = ({contacts, deleteContact, editContact}:Props) => {
+const ContactList = ({contacts, deleteContact, editContact, sortContacts, sort, setSort}:Props) => {
+  const SortColumn = (field: string) => {
+    const asc = 
+      <FaSortUp onClick={()=>{
+        sortContacts("asc",field);
+        setSort({type: "asc", field})
+      }}/>;
+    const desc = 
+      <FaSortDown onClick={()=>{
+        sortContacts("desc",field);
+        setSort({type: "desc", field})
+      }}/>;
+    const individualSort = sort.type === "asc" ? desc : asc;
+
+    const AllSort = <>{asc} {desc}</>
+      
+    return (
+      field === sort.field ? individualSort : AllSort
+    )
+  }
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Contact Number</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-          {contacts.length > 0 ? (
-              contacts.map(contact => (
-              <tr key={contact.id}>
-                  <td>{contact.name}</td>
-                  <td>{contact.email}</td>
-                  <td>{contact.contactNumber}</td>
-                  <td>
-                  <button className="button muted-button" onClick={() => {editContact(contact)}}>Edit</button>
-                  <button className="button muted-button" onClick={() => {deleteContact(contact.id)}}>Delete</button>
-                  </td>
-              </tr>
-              ))
-          ) : (
-              <tr>
-              <td colSpan={3}>No Contacts</td>
-              </tr>
-          )}
-      </tbody>
-    </table>
+    <>
+      <span> Current Sort: {`${sort.field} ${sort.type}`}</span>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Name{ SortColumn("name") }
+            </th>
+            <th>Email{ SortColumn("email") }
+            </th>
+            <th>Contact Number{ SortColumn("contactNumber") }
+            </th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+            {contacts.length > 0 ? (
+                contacts.map(contact => (
+                <tr key={contact.id}>
+                    <td>{contact.name}</td>
+                    <td>{contact.email}</td>
+                    <td>{contact.contactNumber}</td>
+                    <td>
+                    <button className="button muted-button" onClick={() => {editContact(contact)}}>Edit</button>
+                    <button className="button muted-button" onClick={() => {deleteContact(contact.id)}}>Delete</button>
+                    </td>
+                </tr>
+                ))
+            ) : (
+                <tr>
+                <td colSpan={3}>No Contacts</td>
+                </tr>
+            )}
+        </tbody>
+      </table>
+    </>
   )
 }
 
