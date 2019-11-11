ba8b08122ba272da260e08c31d58d55f8e804a6c
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2019-11-11 08:53:18 +0800

    Add validation

--- src/contacts/edit.tsx
+++ src/contacts/edit.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { Contact, initialState } from './model';
+import validateContact, {initialErrorState, validForm} from './validate';
 
 interface Props {
     currentContact: Contact;
@@ -9,10 +10,13 @@ interface Props {
 }
 
 const EditUserForm = ({currentContact,updateContact,setEditing}:Props) => {
-    const [contact, setContact] = useState(currentContact)
+    const [contact, setContact] = useState(currentContact);
+    const [errors, setErrors] = useState(initialErrorState);
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target as HTMLInputElement;
+
+        setErrors(validateContact(errors, name, value));
 
         setContact({ ...contact, [name]: value });
     }
@@ -20,13 +24,15 @@ const EditUserForm = ({currentContact,updateContact,setEditing}:Props) => {
     const handleSubmit = (event:FormEvent<HTMLFormElement>,id:number,contact:Contact) => {
         event.preventDefault();
         if (!contact.name || !contact.email || !contact.contactNumber) return;
+        if (!validForm(errors)) return;
     
         updateContact(id,contact);
         setContact(initialState);
     }
 
     useEffect(() => {
-        setContact(currentContact)
+        setContact(currentContact);
+        setErrors(initialErrorState);
     }, [currentContact])
 
     return (
---
+++ src/contacts/validate.ts
@@ -0,0 +1,47 @@
+export interface ContactError {
+    name: string;
+    email: string;
+    contactNumber: string;
+}
+
+export const initialErrorState: ContactError = {
+    name: '', email: '', contactNumber: '' 
+}
+
+const validEmailRegex = 
+  RegExp(/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i);
+
+const validateLength = (name: string, value: string, length: number) =>
+    value.length < 5 ?
+    `${name} must be ${length} characters long!` : ''
+
+export const validForm = (errors: ContactError) => {
+    return errors.contactNumber === '' && errors.email === '' && errors.name === '';
+}
+
+
+const validate = (errors: ContactError, name: string, value: string) => {  
+    switch (name) {
+        case 'name': 
+            errors.name = 
+            validateLength('Name',value,5);
+            break;
+        case 'email': 
+            errors.email = 
+            validEmailRegex.test(value)
+                ? ''
+                : 'Email is not valid!';
+            break;
+        case 'contactNumber': 
+            errors.contactNumber = 
+            validateLength('Contact Number',value,4);
+            break;
+        default:
+            break;
+    
+    
+    }
+    return errors;
+}
+
+export default validate;
